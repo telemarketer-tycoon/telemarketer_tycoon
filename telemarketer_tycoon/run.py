a23231d6bbb8ee158febcedcf68650c20a2efab5
Sam Stephens <sam@growthintel.com>
2017-04-24 14:21:04 +0100

    Cleaned up print statements

--- telemarketer_tycoon/run.py
+++ telemarketer_tycoon/run.py
@@ -45,9 +45,9 @@ if __name__ == "__main__":
         print(f'\n\n### WEEK {week_no} ###\n\n')
         event_loop.run_for(7)
         print(f'Total calls made: {stat_logger.total_calls_made():,}.\n'
-              f'Total money (before salaries): £{stat_logger.total_money():,}\n')
+              f'Total money (before salaries): £{stat_logger.total_money():,}')
         after_salary = stat_logger.subtract_money(sum(p.wage for p in company.employees))
-        print(f'Total money (after salaries): £{stat_logger.total_money():,}\n')
+        print(f'Total money (after salaries): £{stat_logger.total_money():,}')
 
         if after_salary < 0:
             print('You ran out of money!\n\nGAME OVER\n')
