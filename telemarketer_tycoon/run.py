833d1175ebe1772d07c560a8b93131bfb271e4b6
Dylan Barth <dylan@growthintel.com>
2017-04-26 10:07:53 -0500

    Add intro screen.

--- telemarketer_tycoon/run.py
+++ telemarketer_tycoon/run.py
@@ -2,11 +2,17 @@ import os
 from telemarketer_tycoon.exceptions import GameOver
 from telemarketer_tycoon.game import Game
 from telemarketer_tycoon.ui import GamePrompt
+from telemarketer_tycoon.logo import logo
+
+def play_intro():
+    print(f'\n{logo}\n')
+    print("\n###\nPress any key to start the game.\n###\n")
+    input()
 
 if __name__ == "__main__":
     game = Game()
     prompt = GamePrompt(game)
-
+    play_intro()
     for week_no in range(1, 1000):
         os.system('clear')
         print(f'\n\n### WEEK {week_no} ###\n\n')
